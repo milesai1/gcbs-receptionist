f05021ea99371b98cabfaab27e5d68a92ed83b66
milesai1 <milesredpath5@gmail.com>
2026-03-18 12:59:17 +1000

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,25 @@ require("dotenv").config();
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Receptionist running");
+  res.status(200).send("Receptionist running");
 });
 
-app.listen(PORT, () => {
+app.post("/voice", (req, res) => {
+  const twiml = `<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+  <Say>Hello, thanks for calling Gold Coast Blinds and Shutters. How can I help you today?</Say>
+</Response>`;
+
+  res.type("text/xml");
+  res.send(twiml);
+});
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on ${PORT}`);
 });
